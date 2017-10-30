1306b4d8420030e81bc1b4037b99a9414684ca7d
Fan Hengyuan <fanhengyuan1994@gmail.com>
2017-10-30 09:26:27 +0800

    mobile style

--- footer.php
+++ footer.php
@@ -4,18 +4,18 @@
 
 <!-- 版权说明 -->
 <footer id="footer">
-	<div class="bottom-nav">
-				<?php
-				wp_nav_menu( array(
-					'theme_location'	=> 'header',
-					'menu_class'		=> 'bottom-menu',
-					'fallback_cb'		=> 'default_menu'
-				) );
-			?>
-	</div>		
-	<div id="contentinfo">
-		Copyright © 2007-<?php date_default_timezone_set('PRC'); echo date('Y') ?> <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a> All rights reserved. | Theme by <a  href="https://www.ricemouse.com/27.html" title="米鼠" target="_blank">Mydream</a> | <?php echo get_option( 'zh_cn_l10n_icp_num' );?>
-	</div>
+    <div class="bottom-nav">
+                <?php
+                wp_nav_menu( array(
+                    'theme_location'	=> 'header',
+                    'menu_class'		=> 'bottom-menu',
+                    'fallback_cb'		=> 'default_menu'
+                ) );
+            ?>
+    </div>
+    <div id="contentinfo">
+        Copyright © <?php date_default_timezone_set('PRC'); echo date('Y') ?> <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a> All rights reserved. | Theme by <a  href="https://github.com/fanhengyuan/WpTheme" title="米鼠" target="_blank">Mydream</a> | <?php echo get_option( 'zh_cn_l10n_icp_num' );?>
+    </div>
 </footer>
 
 <!-- 返回顶部 -->
@@ -29,16 +29,16 @@
 
 <!-- 百度分享 -->
 <?php if ( is_single() ) { ?>
-	<?php if (md_get_option('baidu_share')) : ?>
+    <?php if (md_get_option('baidu_share')) : ?>
 <!--<script>window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"1","bdSize":"18"},"share":{"bdSize":18}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];</script> -->
-	<?php else: ?>
-		<script>window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"1","bdSize":"18"},"share":{"bdSize":18}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];</script>
-	<?php endif; ?>	
+    <?php else: ?>
+        <script>window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"1","bdSize":"18"},"share":{"bdSize":18}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];</script>
+    <?php endif; ?>
 <?php } ?>
 
 <!-- 左下播放器 -->
 <?php if (md_get_option('playerbox')) { ?>
-	<?php get_template_part( 'inc/playerbox' ); ?>
+    <?php get_template_part( 'inc/playerbox' ); ?>
 <?php } ?>
 
 <?php wp_footer(); ?>
